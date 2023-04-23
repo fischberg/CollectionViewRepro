672b468f1a4c8e46ae0d227587e0c987d119a231
craig <craig@fischberg.com>
2023-04-23 15:37:50 -0500

    Initial reproduction of non-centered CollectionView and initially selecting item not being highlighted.

---
+++ CollectionViewRepro/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CollectionViewRepro;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ CollectionViewRepro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CollectionViewRepro;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
